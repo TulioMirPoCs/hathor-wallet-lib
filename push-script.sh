1a19552fadd3d394d2c871844a679a653b2987c5
tuliomir <tulio.mir@gmail.com>
2024-06-28 18:52:58 -0300

    feat: fetching proj data

--- push-script.sh
+++ push-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export GH_TOKEN=abc123
+#export GH_TOKEN=abc123
 
 PROJECT_NUMBER=1
 PR_URL=https://github.com/TulioMirPoCs/hathor-wallet-lib/pull/12
@@ -12,36 +12,44 @@ fi
 
 #gh project item-add "$PROJECT_NUMBER" --owner "@me" --url "$PR_URL"
 ORG_NAME="TulioMirPoCs"
+COLUMN_NAME="In Progress"
 QUERY="
-query {
-  organization(login: \"$ORG_NAME\") {
-    projectV2(number: $PROJECT_NUMBER) {
-      id
-      field(name: \"Status\") {
-        __typename
-        ...on ProjectV2SingleSelectField {
-          id
-          options {
-            id
-            name
+          query {
+            organization(login: \"$ORG_NAME\") {
+              projectV2(number: $PROJECT_NUMBER) {
+                id
+                field(name: \"Status\") {
+                  __typename
+                  ...on ProjectV2SingleSelectField {
+                    id
+                    options {
+                      id
+                      name
+                    }
+                  }
+                }
+              }
+            }
           }
-        }
-      }
-    }
-  }
-}
-"
-PROJ_RESPONSE=$(gh api graphql -f query="$QUERY")
-echo "Response: $PROJ_RESPONSE"
+          "
+          PROJ_RESPONSE=$(gh api graphql -f query="$QUERY")
+          echo "Response: $PROJ_RESPONSE"
+          echo "Column Name: $COLUMN_NAME"
 
-echo "$PROJ_RESPONSE" | jq -r '
-  .data.organization.projectV2.id as $projectId |
-  .data.organization.projectV2.field.id as $fieldId |
-  .data.organization.projectV2.field.options[] |
-  select(.name == "In Progress") |
-  {
-    projectId: $projectId,
-    fieldId: $fieldId,
-    optionId: .id
-  }
-  '
+          PROJ_DATA=$(echo "$PROJ_RESPONSE" | jq -r --arg column_name "$COLUMN_NAME" '
+            .data.organization.projectV2.id as $projectId |
+            .data.organization.projectV2.field.id as $fieldId |
+            .data.organization.projectV2.field.options[] |
+            select(.name == $column_name) |
+            {
+              projectId: $projectId,
+              fieldId: $fieldId,
+              optionId: .id
+            }')
+          PROJECT_ID=$(echo "$PROJ_DATA" | jq -r '.projectId')
+          FIELD_ID=$(echo "$PROJ_DATA" | jq -r '.fieldId')
+          OPTION_ID=$(echo "$PROJ_DATA" | jq -r '.optionId')
+
+          echo "PROJECT_ID: $PROJECT_ID"
+          echo "FIELD_ID: $FIELD_ID"
+          echo "OPTION_ID: $OPTION_ID"
