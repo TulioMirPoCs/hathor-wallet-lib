5a3f64b38b1bfb52b785b3ef4cd9326817079afc
tuliomir <tulio.mir@gmail.com>
2024-06-28 13:01:15 -0300

    feat: fetching project data

--- push-script.sh
+++ push-script.sh
@@ -2,7 +2,7 @@
 export GH_TOKEN=abc123
 
 PROJECT_NUMBER=1
-PR_URL=$1
+PR_URL=https://github.com/TulioMirPoCs/hathor-wallet-lib/pull/12
 
 # Check if both arguments are provided
 if [ -z "$PROJECT_NUMBER" ] || [ -z "$PR_URL" ]; then
@@ -10,4 +10,38 @@ if [ -z "$PROJECT_NUMBER" ] || [ -z "$PR_URL" ]; then
     return 1
 fi
 
-gh project item-add "$PROJECT_NUMBER" --owner "@me" --url "$PR_URL"
+#gh project item-add "$PROJECT_NUMBER" --owner "@me" --url "$PR_URL"
+ORG_NAME="TulioMirPoCs"
+QUERY="
+query {
+  organization(login: \"$ORG_NAME\") {
+    projectV2(number: $PROJECT_NUMBER) {
+      id
+      field(name: \"Status\") {
+        __typename
+        ...on ProjectV2SingleSelectField {
+          id
+          options {
+            id
+            name
+          }
+        }
+      }
+    }
+  }
+}
+"
+PROJ_RESPONSE=$(gh api graphql -f query="$QUERY")
+echo "Response: $PROJ_RESPONSE"
+
+echo "$PROJ_RESPONSE" | jq -r '
+  .data.organization.projectV2.id as $projectId |
+  .data.organization.projectV2.field.id as $fieldId |
+  .data.organization.projectV2.field.options[] |
+  select(.name == "In Progress") |
+  {
+    projectId: $projectId,
+    fieldId: $fieldId,
+    optionId: .id
+  }
+  '
